9e3bf1f76846ae32c72406237ca758bb6a22eefe
serversepp <sese.bentele@gmaIL.COM>
2021-04-28 17:38:15 +0200

    Division geaddet

--- Test1.c
+++ Test1.c
@@ -2,3 +2,14 @@
 // Created by serversepp on 28.04.21.
 //
 
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+    float x,y,z = 0.0f;
+    scanf("%f", &x);
+    scanf("%f", &y);
+
+    printf("%f / %f gibt %f ",x,y, x/y);
+
+}
